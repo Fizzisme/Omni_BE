e8f8e961aa58de59435ce0dcedd4acff50973b8c
Fizzisme <nguyenetuanphi12c1.2022@gmail.com>
2026-04-10 22:48:54 +0700

    Test

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -21,6 +21,7 @@ const update = async (req, res, next) => {
 
 const getUserById = async (req, res, next) => {
     try {
+        console.log(req.params.id);
         const result = await userService.getUserById(req.params.id);
         res.status(StatusCodes.OK).json(result);
     } catch (err) {
